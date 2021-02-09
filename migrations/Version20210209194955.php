8a4ac27d665906a287dc8b48696abdf3e61162da
Daniel <daniel@debian.home>
2021-02-09 14:53:32 -0500

    Added Unique constraint on votes (member_id, nomination_id)

---
+++ migrations/Version20210209194955.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210209194955 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER INDEX idx_872662181d650ba4 RENAME TO IDX_872662187597D3FE');
+        $this->addSql('CREATE UNIQUE INDEX vote_unique ON vote (member_id, nomination_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER INDEX idx_872662187597d3fe RENAME TO idx_872662181d650ba4');
+        $this->addSql('DROP INDEX vote_unique');
+    }
+}
